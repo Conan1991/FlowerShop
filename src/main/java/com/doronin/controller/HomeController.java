1bb77f8ba4e3b8a4d59a047dc7af549297ca0abc
Vitaliy Doronin <vitaliy.doronin@accenture.com>
2020-03-10 11:37:21 +0300

    remove project files

--- src/main/java/com/doronin/controller/HomeController.java
+++ src/main/java/com/doronin/controller/HomeController.java
@@ -2,6 +2,8 @@ package com.doronin.controller;
 
 import java.util.Locale;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController 
 {
+	private static final Logger LOGGER = LogManager.getLogger(HomeController.class);
+
 	@GetMapping("/home")
 	public String homeInit(Locale locale, Model model) {
+		LOGGER.info(model.toString());
 		return "home";
 	}
 }
--- target/FlowerShop-0.0.1-SNAPSHOT/WEB-INF/views/home.jsp
+++
@@ -1,7 +0,0 @@
-<html>
-<head><title>Spring 5 Web MVC Example</title></head>
-<body>
-<h1>Test</h1>
-<h2>Example header</h2>
-</body>
-</html>
